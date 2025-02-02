79a5da126ddde71c9a20505f9e71f436073ef91f
Rahul Mahra <147267603+Rahul-Mahra118@users.noreply.github.com>
2025-02-02 09:51:01 +0530

    Update index.js

--- Generatepassword/index.js
+++ Generatepassword/index.js
@@ -1,10 +1,29 @@
-function generatePassword(length, includeNumbers,includeSpecialChars){
-    let getPassword="";
-    for(let i=0;i<length;i++){
-        const getIndex= Math.floor(Math.random()*10);
-        
+function generatePassword(length, includeNumbers, includeSpecialChars) {
+    let getPassword = "";
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%^&*()_-+=<>?";
 
+    let charset = letters; 
 
+    if (includeNumbers) {
+        charset += numbers;
     }
 
+    if (includeSpecialChars) {
+        charset += specialChars; 
+    }
+    const charsetLength = charset.length;
+    for (let i = 0; i < length; i++) {
+        const getIndex = Math.floor(Math.random() * charsetLength); 
+        getPassword += charset[getIndex];
+    }
+
+    return getPassword;
 }
+const passwordLength = 12; 
+const includeNumbers = true; 
+const includeSpecialChars = true; 
+
+const password = generatePassword(passwordLength, includeNumbers, includeSpecialChars);
+console.log(password);
