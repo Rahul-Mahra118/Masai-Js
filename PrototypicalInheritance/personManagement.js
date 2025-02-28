56293b25cdff8c476964eed3142e28fe847dce65
Rahul-Mahra118 <mahrarahul80@gmail.com>
2025-02-28 14:19:38 +0530

    added

--- PrototypicalInheritance/personManagement.js
+++ PrototypicalInheritance/personManagement.js
@@ -10,6 +10,17 @@ Person.prototype.introduce=function(){
 }
 
 //constructor function
-function Employee(){
-    
+function Employee(name,age,title){
+  Person.call(this, name, age);
+
+  this.jobTitle = title; 
+
 }
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.work=function(){
+  console.log(`${this.name} is working as a ${this.jobTitle}`)
+}
+
+let person= new Person("Rahul",23);
+let employ= new Employee("Shivam", 23, "Web Developer")
+employ.work()
