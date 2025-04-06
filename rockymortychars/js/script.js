5dcee628a87772c9cb6c986f5ee66dd5696d7a99
Rahul-Mahra118 <mahrarahul80@gmail.com>
2025-04-06 22:51:45 +0530

    added

---
+++ rockymortychars/js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded',async()=>{
+
+
+   let Container= document.getElementById('showCharacters');
+   let Characters= [];
+    let URL='https://rickandmortyapi.com/api/character';
+    async function fetchData(){
+        let response= await fetch(URL);
+        let data= await response.json();
+        Characters= data.results;
+        
+        displayData()
+    }
+    fetchData()
+    // Character image
+// Character name
+// Species
+// Status (Alive, Dead, Unknown)
+    function displayData(){
+        Container.innerHTML='';
+        Characters.forEach((character)=>{
+            const characterCard=document.createElement('div');
+            characterCard.classList.add('character-card');
+            characterCard.innerHTML=
+            `  <img src="${character.image}" alt="character-img">
+               <h2>Name: ${character.name}</h2>
+               <p>Species: ${character.species} </p>
+               <p>Status: ${character.status}</p>
+            `
+            Container.appendChild(characterCard);
+        })
+
+    }
+})
