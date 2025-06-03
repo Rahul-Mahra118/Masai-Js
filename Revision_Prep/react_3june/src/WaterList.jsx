56903c84a87504b56bf28add47ac665f9de7758a
Geek-11 <geekstars0110@gmail.com>
2025-06-04 02:13:55 +0530

    added

--- Revision_Prep/react_3june/src/WaterList.jsx
+++ Revision_Prep/react_3june/src/WaterList.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect ,useRef} from "react";
 
 import { getTodayTotal } from "./utils";
 
 const WaterList = ({ logs, setLogs }) => {
+  const ref= useRef()
   const handleRemove = (id) => {
     const logi = logs.filter((log) => log.id !== id);
     //findIndex or splice
     setLogs(logi);
   };
-  const value=getTodayTotal(logs)
+
+  const value=parseInt(getTodayTotal(logs))
+  {console.log('value',value)}
+  useEffect(()=>{
+    console.log(ref.current.value)
+    const value1= +(ref.current.textContent.split('-')[1])
+    if(value1>3000){
+      ref.current.style.backgroundColor = "red";
+    }
+  },[value])
   return (
     <div style={{ display: "grid" }}>
-        <h1>Todays Intake-{value} </h1>
+        <h1 ref={ref}>Todays Intake-{value} </h1>
+
       {logs.map((log, idx) => (
         <div key={idx + 1} style={{ border: "1px solid black " }}>
           <h2>Water Amount:{log.amount}</h2>
